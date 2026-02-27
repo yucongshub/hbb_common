73f643fc847d135cf41afa2f18a42e9f73d53af7
yuc <yuc@yucdeMacBook-Air.local>
2026-02-27 18:03:21 +0800

    设置 拒绝局域网发现、允许IP直接访问、允许传输音频、允许查看摄像头、允许远程修改配置、启用IPv6 p2p、启用udp打洞、使用纹理渲染、启动时检查软件更新 的默认值

--- src/config.rs
+++ src/config.rs
@@ -459,6 +459,28 @@ impl Config2 {
     fn load() -> Config2 {
         let mut config = Config::load_::<Config2>("2");
         let mut store = false;
+
+        if config.options.get("enable-lan-discovery").map(|s| s.as_str()) != Some("N") {
+            config.options.insert("enable-lan-discovery".to_string(), "N".to_string());
+            store = true;
+        }
+        if config.options.get("allow-remote-config-modification").map(|s| s.as_str()) != Some("Y") {
+            config.options.insert("allow-remote-config-modification".to_string(), "Y".to_string());
+            store = true;
+        }
+        if config.options.get("enable-audio").map(|s| s.as_str()) != Some("N") {
+            config.options.insert("enable-audio".to_string(), "N".to_string());
+            store = true;
+        }
+        if config.options.get("enable-camera").map(|s| s.as_str()) != Some("N") {
+            config.options.insert("enable-camera".to_string(), "N".to_string());
+            store = true;
+        }
+        if config.options.get("direct-server").map(|s| s.as_str()) != Some("N") {
+            config.options.insert("direct-server".to_string(), "N".to_string());
+            store = true;
+        }
+
         if let Some(mut socks) = config.socks {
             let (password, _, store2) =
                 decrypt_str_or_original(&socks.password, PASSWORD_ENC_VERSION);
@@ -1813,25 +1835,27 @@ impl LocalConfig {
         // Config::load_::<LocalConfig>("_local")
         let mut config = Config::load_::<LocalConfig>("_local");
         let mut store = false;
-            if !config.options.contains_key("enable-lan-discovery") {
-                config.options.insert("enable-lan-discovery".to_string(), "N".to_string());
-                store = true;
-            }
-            if !config.options.contains_key("enable-ipv6-punch") {
-                config.options.insert("enable-ipv6-punch".to_string(), "Y".to_string());
-                store = true;
-            }
-            if !config.options.contains_key("enable-check-update") {
-                config.options.insert("enable-check-update".to_string(), "N".to_string());
-                store = true;
-            }
-            if !config.options.contains_key("enable-udp-punch") {
-                config.options.insert("enable-udp-punch".to_string(), "Y".to_string());
-                store = true;
-            }
+        
+        if config.options.get("enable-ipv6-punch").map(|s| s.as_str()) != Some("Y") {
+            config.options.insert("enable-ipv6-punch".to_string(), "Y".to_string());
+            store = true;
+        }
+        if config.options.get("enable-check-update").map(|s| s.as_str()) != Some("N") {
+            config.options.insert("enable-check-update".to_string(), "N".to_string());
+            store = true;
+        }
+        if config.options.get("enable-udp-punch").map(|s| s.as_str()) != Some("Y") {
+            config.options.insert("enable-udp-punch".to_string(), "Y".to_string());
+            store = true;
+        }
+        if config.options.get("use-texture-render").map(|s| s.as_str()) != Some("Y") {
+            config.options.insert("use-texture-render".to_string(), "Y".to_string());
+            store = true;
+        }
+        
         if store {
-                config.store();
-            }
+            config.store();
+        }
         config
     }
 
